44436af6d91dfc2ed9f2047b3ff5939cd32c9d2e
Sergey Makarov <makar1031@yandex.ru>
2017-10-01 13:13:23 +0300

    выполнение задания

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const $ = require('jquery')
 require('./app.css')
-$(document).ready(function() {
-    $('#add-todo').on('click', function() {
+$(document).ready(() => {
+    var newItemHandler = () => {
         var title = $('#title').val();
+        if (title === '') {
+            alert('Необходимо ввести значение');
+            return;
+        }
         $('#title').val('');
-        $('#todos').append('<li>' + title + '</ll>');
+        var item = $("<li></li>").text(title);
+        var priorityButton = $('<button></button>').text('Priority');
+        priorityButton.addClass('btn priority');
+        priorityButton.click(() => {
+            item.toggleClass('priority-task');
+        });
+        var removeButton = $('<button></button>').text('Remove');
+        removeButton.addClass('btn remove');
+        removeButton.click(() => {
+            item.hide('slow', () => {
+                item.remove();
+            });
+        });
+        item.append(priorityButton);
+        item.append(removeButton);
+        item.fadeOut(0);
+        $('#todos').append(item);
+        item.fadeIn();
+    }
+    $('#title').keypress(e => {
+        if (e.which == 13) {
+            newItemHandler();
+        }
     })
+    $('#add-todo').click(newItemHandler);
 });
